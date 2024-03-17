e7e80bb385eaef4a339bb26a25beb67dfce0bdb9
s27611 <s27611@pjwstk.edu.pl>
2024-03-17 13:17:19 +0100

    dodano feature-max

---
+++ ZadanieNr2/.idea/.idea.ZadanieNr2/.idea/Feature-average/Feature-average/FeatureMax.cs
@@ -0,0 +1,25 @@
+﻿namespace Feature_average;
+
+public class FeatureMax
+{
+    public static int FindMaxValue(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta lub niezainicjowana.");
+        }
+
+        int max = array[0];
+        foreach (int num in array)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+    
+    
+}
