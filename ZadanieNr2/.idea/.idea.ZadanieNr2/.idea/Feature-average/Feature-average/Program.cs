c768ed4c7e74544e383dc4252f461f51cca0f10e
s27611 <s27611@pjwstk.edu.pl>
2024-03-17 13:11:17 +0100

    doadno feature_average

---
+++ ZadanieNr2/.idea/.idea.ZadanieNr2/.idea/Feature-average/Feature-average/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Feature_average;
+
+public static class FeatureAverage
+{
+    public static double Average(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta lub niezainicjowana.");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
